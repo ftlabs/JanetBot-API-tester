1b09749d118f90c30586507ca752530afa75d661
Chris Gathercole <chris.gathercole@ft.com>
2018-03-19 16:03:22 +0000

    re-scale the boxes if the image was pre-shrunk before processing

--- client/js/main.js
+++ client/js/main.js
@@ -9,7 +9,7 @@ function init() {
 function parseInput(e) {
 	e.preventDefault();
 	var value = e.currentTarget.querySelector('#json-input').value;
-	
+
 	if(value.length > 0) {
 		try {
 			tempResult = JSON.parse(value);
@@ -57,6 +57,16 @@ function parseInput(e) {
 }
 
 function getImage(imgData, canvas) {
+
+	let xShrunkBy = 1.0;
+	let yShrunkBy = 1.0;
+
+	// added to re-scale the boxes to the correct size if the classifyImage service shrank the image before processing
+	if (imgData.hasOwnProperty('shrunk')) {
+		xShrunkBy = imgData.shrunk.to.width  / imgData.shrunk.from.width ;
+		yShrunkBy = imgData.shrunk.to.height / imgData.shrunk.from.height;
+	}
+
 	var image = new Image(imgData.image.shape[0], imgData.image.shape[1]);
 	image.onload = drawImageAtSize;
 	image.src = imgData.image.url;
@@ -68,17 +78,20 @@ function getImage(imgData, canvas) {
 		canvas.height = this.naturalHeight;
 		context.drawImage(this, 0, 0);
 
-		drawSquares(imgData.faces, context);
+		drawSquares(imgData.faces, context, xShrunkBy, yShrunkBy);
 	}
 
 }
 
-function drawSquares(faces, context) {
+function drawSquares(faces, context, xShrunkBy=1.0, yShrunkBy=1.0) {
 	for(var i = 0; i < faces.length; ++i) {
 		var pos = faces[i].coordinates;
 		context.strokeStyle = (faces[i].gender === "man")?"blue":"red";
 		context.lineWidth = 2;
-		context.strokeRect(pos.point[0],pos.point[1],pos.width,pos.height);
+		context.strokeRect(
+			pos.point[0]/xShrunkBy, pos.point[1]/yShrunkBy,
+			pos.width   /xShrunkBy, pos.height  /yShrunkBy
+		);
 	}
 }
 
